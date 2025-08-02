904f58fa4f2090d88bcab2c2d05ef18feb643020
raihanmd <mraihanf471@gmail.com>
2025-08-02 17:06:39 +0700

    ch 1 through official docs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::{env, process};
 
+use rsgrep::{Config, run};
+
 fn main() {
     let args = env::args().collect::<Vec<String>>();
 
-    let config = rsgrep::Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("{err}");
         process::exit(1);
     });
 
-    if let Err(e) = rsgrep::run(config) {
+    if let Err(e) = run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
